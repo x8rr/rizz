706fe12d0984222354552b773ed7b33b22d70ae4
cobblesteve-dev <channelediting521@gmail.com>
2024-06-10 11:33:57 -0500

    add a randomrizz functionality

--- main.js
+++ main.js
@@ -37,3 +37,47 @@ function changeTheme(filename) {
     var stylesheet = document.getElementById('theme-stylesheet');
     stylesheet.href = filename;
 }
+
+function randomRizzler() {
+    var randomRizz = '';
+
+    let randomNumber = Math.floor(Math.random() * 10);
+
+    switch (randomNumber) {
+        case 0:
+            document.getElementById('randomrizz').textContent = 'You\'re in your bed, I\'m in mine.\nOne of us is in the wrong place';
+            break;
+        case 1:
+            document.getElementById('randomrizz').textContent = 'Before I came there was no D in you. That will change'
+            break;
+        case 2:
+            document.getElementById('randomrizz').textContent = 'Do you want to make some twin towers?'
+            break;
+        case 3:
+            document.getElementById('randomrizz').textContent = 'Are you a crime scene?? Because I can\'t resist leaving my DNA all over you.'
+            break;
+        case 4:
+            document.getElementById('randomrizz').textContent = 'Are you an artist? Because you\'re really good at drawing me in ;)'
+            break;
+        case 5:
+            document.getElementById('randomrizz').textContent = 'Are you a light switch? Because you turn me on!'
+            break;
+        case 6:
+            document.getElementById('randomrizz').textContent = 'Are you a time traveler? Because I can see you in my future.'
+            break;
+        case 7:
+            document.getElementById('randomrizz').textContent = 'Are you Google? Because you\'re everything I\'ve been searching for.'
+            break;
+        case 8:
+            document.getElementById('randomrizz').textContent = 'If you were a vegetable, you\'d be a CUTEcumber!'
+            break;
+        case 9:
+            document.getElementById('randomrizz').textContent = 'Are you a bank loan? Because you have got my intrest!'
+            break;
+        case 10:
+            document.getElementById('randomrizz').textContent = 'Are you a camera? Because every time I look at you, I smile.'
+            break;
+        case 11:
+            document.getElementById('randomrizz').textContent = 'Are you a dictionary? Cause you\'re adding meaning to my life.'
+    }
+}
